322712c96f181ca910b6b9695b6197c8213559d1
Bram ten Lohuis <bramtenlohuis@gmail.com>
2016-12-27 17:02:58 +0100

    Some movement in the carousel :P

--- design/parallax-template/js/init.js
+++ design/parallax-template/js/init.js
@@ -6,4 +6,10 @@
     $('.carousel.carousel-slider').carousel({full_width: true});
 
   }); // end of document ready
+  $(function nextSlide() {
+    setTimeout(function () {
+      $('.carousel').carousel('next');
+      nextSlide();
+    }, 10000);
+  })
 })(jQuery); // end of jQuery name space
